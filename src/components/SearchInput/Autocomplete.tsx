5145f58c76c3fb2fc695b535f33d8c76bfd1fc94
Dekker <dekker25@gmail.com>
2019-01-29 12:29:04 +0300

    feature(front): implement keyboard navigation

---
+++ src/components/SearchInput/Autocomplete.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import * as cn from 'classnames';
+import { AutocompleteCard } from '../../entities/AutocompleteCard';
+
+import './Autocompletion.scss';
+
+export interface Props {
+    cards: AutocompleteCard[];
+    onAutocomplete: (name: string) => void;
+}
+
+interface State {
+    selected?: number;
+}
+
+export class Autocomplete extends React.Component<Props, State> {
+    constructor (props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    private onKeyDown (e: KeyboardEvent) {
+        const { selected } = this.state;
+        const { cards, onAutocomplete } = this.props;
+
+        switch (e.key) {
+            case 'ArrowUp':
+                selected > 0 && this.setState({ ...this.state, selected: selected - 1 });
+                break;
+            case 'ArrowDown':
+                if (Number.isInteger(selected)) {
+                    selected < cards.length - 1 && this.setState({ ...this.state, selected: selected + 1 });
+                } else {
+                    this.setState({ ...this.state, selected: 0 });
+                }
+                break;
+            case 'Enter':
+                if (Number.isInteger(selected)) {
+                    onAutocomplete(cards[selected].name);
+                }
+                break;
+        }
+    }
+
+    private onKeyDownBound = this.onKeyDown.bind(this);
+
+    componentDidMount () {
+        document.addEventListener<'keydown'>('keydown', this.onKeyDownBound);
+    }
+
+    componentWillUnmount () {
+        document.removeEventListener<'keydown'>('keydown', this.onKeyDownBound);
+    }
+
+    render () {
+        const { cards, onAutocomplete } = this.props;
+        const { selected } = this.state;
+
+        return (<div className="autocompletion">
+            {
+                cards.map((card, i) =>
+                    <div
+                        className={ cn('autocompletion--card', { 'autocompletion__active': i === selected }) }
+                        key={ i }
+                        onClick={ () => onAutocomplete(card.name) }>
+                        <div className="autocompletion--card-name">{ card.name }</div>
+                        <div className="autocompletion--card-text">{ card.text }</div>
+                    </div>)
+            }
+        </div>);
+    }
+}
--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import * as cn from 'classnames';
-import { ChangeEvent, KeyboardEvent } from 'react';
 
 import './SearchInput.scss';
 import { AutocompleteCard } from '../../entities/AutocompleteCard';
+import { ChangeEvent } from 'react';
+import { Autocomplete } from './Autocomplete';
 
 export interface Props {
     onTextChanged: (text: string) => void;
-    onSearchRequested: (text: string) => void;
+    onSearchRequested: (text: string, isAutocomplete: boolean) => void;
     autocompletion?: Record<string, AutocompleteCard>;
     inititalText?: string;
 }
@@ -19,7 +20,7 @@ interface State {
 export class SearchInput extends React.PureComponent<Props, State> {
     private inputRef: React.RefObject<HTMLInputElement>;
 
-    constructor(props) {
+    constructor (props) {
         super(props);
 
         this.state = {
@@ -35,34 +36,45 @@ export class SearchInput extends React.PureComponent<Props, State> {
         this.props.onTextChanged(value);
     };
 
-    private onKeyPressed = (e) => {
-        if ((e as KeyboardEvent).key === 'Enter') {
-            this.handleSearchRequest(this.state.text);
+    private onKeyPressed = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        const { autocompletion } = this.props;
+
+        switch (e.key) {
+            case 'Enter':
+                this.handleSearchRequest(this.state.text);
+                break;
+            case 'ArrowDown':
+            case 'ArrowUp':
+                if (autocompletion) {
+                    this.inputRef.current.blur();
+                    e.preventDefault();
+                }
+                break;
         }
     };
 
     private handleSearchRequest = (text: string) => {
-        this.props.onSearchRequested(text);
+        this.props.onSearchRequested(text, false);
     };
 
     private onAutocomplete = (text: string) => {
         this.setState({ ...this.state, text });
-        this.props.onSearchRequested(text);
+        this.props.onSearchRequested(text, true);
     };
 
     private onClear = () => {
-         this.setState({ ...this.state, text: '' });
-         this.inputRef.current.focus();
-    }
+        this.setState({ ...this.state, text: '' });
+        this.inputRef.current.focus();
+    };
 
-    render() {
+    render () {
         const { autocompletion } = this.props;
         const { text } = this.state;
         let aucompleteCards: AutocompleteCard[] = [];
         if (autocompletion) {
             const keys = Object.keys(autocompletion);
             for (let key of keys) {
-                aucompleteCards.push(autocompletion[key]);
+                aucompleteCards.push(autocompletion[ key ]);
             }
         }
 
@@ -70,33 +82,27 @@ export class SearchInput extends React.PureComponent<Props, State> {
             <div className="search-container">
                 <div className="search-panel">
                     <label className="search-input" htmlFor="search-input">
-                        <input value={text}
-                            onChange={(e) => this.onInput(e)}
-                            onKeyPress={(e) => this.onKeyPressed(e)}
-                            id="search-input"
-                            ref={this.inputRef}
-                            required />
+                        <input value={ text }
+                               onChange={ (e) => this.onInput(e) }
+                               onKeyDown={ (e) => this.onKeyPressed(e) }
+                               id="search-input"
+                               ref={ this.inputRef }
+                               required/>
                         <div className="search-label">
                             <div className="search-label--placeholder">Search</div>
                         </div>
                     </label>
-                    {<div className={cn("search-cross", { 'search-cross--hidden': !Boolean(text) })} onClick={() => this.onClear()}>&times;</div>}
-                    <button className="search-button" onClick={() => this.handleSearchRequest(this.state.text)}>
+                    { <div className={ cn('search-cross', { 'search-cross--hidden': !Boolean(text) }) }
+                           onClick={ () => this.onClear() }>&times;</div> }
+                    <button className="search-button" onClick={ () => this.handleSearchRequest(this.state.text) }>
                         <div className="search-button--icon">
-                            <i className="icon-search" />
+                            <i className="icon-search"/>
                         </div>
                     </button>
                 </div>
                 {
-                    Boolean(text) && aucompleteCards.length > 0 && <div className="autocompletion">
-                        {
-                            aucompleteCards.map((card, i) =>
-                                <div className="autocompletion--card" onClick={() => this.onAutocomplete(card.name)}>
-                                    <div className="autocompletion--card-name">{card.name}</div>
-                                    <div className="autocompletion--card-text">{card.text}</div>
-                                </div>)
-                        }
-                    </div>
+                    Boolean(text) && aucompleteCards.length > 0 &&
+                    <Autocomplete onAutocomplete={ this.onAutocomplete.bind(this) } cards={ aucompleteCards }/>
                 }
             </div>
         );
