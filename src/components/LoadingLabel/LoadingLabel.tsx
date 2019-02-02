1445b4f00454821d58657d14d84e7c753ce2703d
Dekker <dekker25@gmail.com>
2019-02-02 14:13:23 +0300

    markup(front): loading padding

--- src/components/LoadingLabel/LoadingLabel.tsx
+++ src/components/LoadingLabel/LoadingLabel.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 
 import './LoadingLabel.scss';
 
-export const LoadingLabel = () => <div className="loading-container">
-    <div className="loading"><i className="icon-spinner8 icon-big icon-rotating"/>Loading</div>
+export const LoadingLabel = () => <div className="loading__container">
+    <div className="loading__spinner"><i className="icon-spinner8 icon-big icon-rotating"/>Loading</div>
 </div>;
