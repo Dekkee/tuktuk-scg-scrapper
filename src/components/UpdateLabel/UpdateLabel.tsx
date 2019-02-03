b8602dfa2798762120bfbdca34c7d32f6c718012
Dekker <dekker25@gmail.com>
2019-02-03 10:17:47 +0300

    markup(front): minor refactor update label

--- src/components/UpdateLabel/UpdateLabel.tsx
+++ src/components/UpdateLabel/UpdateLabel.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as cn from 'classnames';
 
 import './UpdateLabel.scss';
+import { Button } from '../Button';
 
 export enum UpdateStatus {
     Required,
@@ -27,28 +28,34 @@ export class UpdateLabel extends React.PureComponent<Props> {
         const isHidden = status === UpdateStatus.NotRequired || status === UpdateStatus.Cancelled;
         const isFailed = status === UpdateStatus.Failed;
         return (
-            <div className={cn(
-                "update-label-container",
-                {"update-label-container--hidden": isHidden},
-                {"update-label-container--failed": isFailed},
-                )}>
+            <div className={ cn(
+                'update-label-container',
+                { 'update-label-container--hidden': isHidden },
+                { 'update-label-container--failed': isFailed },
+            ) }>
                 {
                     status === UpdateStatus.Required &&
-                    <span className="update-label update-label--ready" onClick={onRequestUpdate}>Update is ready.<br/>Click me to start updating!</span>
+                    <div className="update-label update-label--ready">
+                        <div className="update-label__text">Update is ready.</div>
+                        <Button label="Refresh" onClick={ onRequestUpdate }/>
+                    </div>
                 }
                 {
                     status === UpdateStatus.Updating &&
-                    <div className="update-label update-label--updating"><i className="icon-spinner8 icon-big icon-rotating" />Loading update!</div>
+                    <div className="update-label update-label--updating"><i
+                        className="icon-spinner8 icon-big icon-rotating"/>Loading update!</div>
                 }
                 {
                     status === UpdateStatus.Failed &&
-                    <div className="update-label update-label--failed"><i className="icon-sad icon-big" />Update is failed!</div>
+                    <div className="update-label update-label--failed"><i className="icon-sad icon-big"/>Update
+                        is failed!</div>
                 }
                 {
                     !isHidden &&
-                    <div className="update-label update-label__close" onClick={onUpdateCancelled}>&times;</div>
+                    <div className="update-label update-label__close"
+                         onClick={ onUpdateCancelled }>&times;</div>
                 }
             </div>
-        )
+        );
     }
 }
