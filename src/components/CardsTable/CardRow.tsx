1f9dd14c4d0d63859f9fcc988c03a3d4cc3ce5a0
Dekker <dekker25@gmail.com>
2019-10-13 12:41:15 +0300

    design(details): details refactored

--- src/components/CardsTable/CardRow.tsx
+++ src/components/CardsTable/CardRow.tsx
@@ -15,21 +15,21 @@ export class CardRow extends React.PureComponent<Props> {
         const { name, set, cards } = this.props.card;
         const cardId = encodeURIComponent(name.href.match(/product\/(.*)$/)[1]);
         return <>
-            <Link to={`/card/${cardId}`} className="card-layout__header"
+            <Link to={`/card/${cardId}`} className="card-row__header"
                   style={ { gridRow: `span ${ cards.length }` } }>
-                <div className="card-layout__name">{ name.value }</div>
-                <div className="card-layout__set">{ set.value }</div>
+                <div className="card-row__name">{ name.value }</div>
+                <div className="card-row__set">{ set.value }</div>
             </Link>
             {
                 cards.map(({ condition, price, stock }, i) => <React.Fragment key={ i }>
                         <span>{ condition.value }</span>
-                        <span className="card-layout__price">
+                        <span className="card-row__price">
                         {
                             price && price.slice(0, -1).map((p, i) =>
-                                <div className={ cn('card-layout__discount') } key={ i }>{ p }</div>)
+                                <div className={ cn('card-row__discount') } key={ i }>{ p }</div>)
                         }
                             <div>{ price && price[price.length - 1] }</div>
-                            { isNaN(Number(stock)) && <div className="card-layout__stock">{ stock }</div> }
+                            { isNaN(Number(stock)) && <div className="card-row__stock">{ stock }</div> }
                     </span>
                     </React.Fragment>
                 )
