6ddb843c2c0fc81786168fc083db23d1bb419ce4
Dekker <dekker25@gmail.com>
2020-01-09 18:58:17 +0300

    feat: github tag tool

---
+++ tools/tag.js
@@ -0,0 +1,16 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const packageJson = require('../package.json');
+
+async function applyTags() {
+    const { version } = packageJson;
+    const commitHash = (await exec(`git rev-parse HEAD`)).stdout.slice(0, 6);
+    const buildNumber = process.env.BUILD_NUMBER || commitHash || 'local';
+
+    console.log(`setting tag: ${version}-${buildNumber}`);
+    await exec(`git tag ${version}-${buildNumber}`);
+    await exec(`git push origin ${version}-${buildNumber}`);
+    console.log(`tag ${version}-${buildNumber} pushed`);
+}
+
+applyTags();
