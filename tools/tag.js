71a1263050e8e438086cf3e25e2321cfc5f8ff6c
Alexander Tihoniuk <dekker25@gmail.com>
2020-01-09 20:34:06 +0300

    Tags and release tool (#23)

---
+++ tools/tag.js
@@ -0,0 +1,20 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const packageJson = require('../package.json');
+
+async function applyTags() {
+    const { version } = packageJson;
+    const commitHash = (await exec(`git rev-parse HEAD`)).stdout.slice(0, 6);
+    const buildNumber = process.env.BUILD_NUMBER || commitHash || 'local';
+
+    console.log(`setting tag: ${version}-${buildNumber}`);
+    await exec(`git tag ${version}-${buildNumber}`);
+    await exec(`git push origin ${version}-${buildNumber}`);
+    await exec(`docker push registry.dekker.gdn/tuktuk-scg-scrapper:stable`);
+    await exec(
+        `docker push registry.dekker.gdn/tuktuk-scg-scrapper:${version}-${buildNumber}`
+    );
+    console.log(`tag ${version}-${buildNumber} pushed`);
+}
+
+applyTags();
