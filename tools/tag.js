e1f4cc5f35225b8bdc4f641bd4323c8ea1ffe2e1
Dekker <dekker25@gmail.com>
2020-01-10 02:06:26 +0300

    chore: minor script refactor

--- tools/tag.js
+++ tools/tag.js
@@ -2,23 +2,23 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const packageJson = require('../package.json');
 
+const dockerPackageName = 'registry.dekker.gdn/tuktuk-scg-scrapper';
+
 async function applyTags() {
     const { version } = packageJson;
     const commitHash = (await exec(`git rev-parse HEAD`)).stdout.slice(0, 6);
     const buildNumber = process.env.BUILD_NUMBER || commitHash || 'local';
 
-    console.log(`setting tag: ${version}-${buildNumber}`);
-    // await exec(`git tag ${version}-${buildNumber}`);
-    // await exec(`git push origin ${version}-${buildNumber}`);
-    await exec(
-        `docker tag registry.dekker.gdn/tuktuk-scg-scrapper:latest registry.dekker.gdn/tuktuk-scg-scrapper:stable`
-    );
-    await exec(`docker push registry.dekker.gdn/tuktuk-scg-scrapper:stable`);
-    await exec(
-        `docker tag registry.dekker.gdn/tuktuk-scg-scrapper:latest registry.dekker.gdn/tuktuk-scg-scrapper:${version}-${buildNumber}`
-    );
-    await exec(`docker push registry.dekker.gdn/tuktuk-scg-scrapper:${version}-${buildNumber}`);
-    console.log(`tag ${version}-${buildNumber} pushed`);
+    const fullVersion = `${version}-${buildNumber}`;
+
+    console.log(`setting tag: ${fullVersion}`);
+    // await exec(`git tag ${fullVersion}`);
+    // await exec(`git push origin ${fullVersion}`);
+    await exec(`docker tag ${dockerPackageName}:latest ${dockerPackageName}:stable`);
+    await exec(`docker push ${dockerPackageName}:stable`);
+    await exec(`docker tag ${dockerPackageName}:latest ${dockerPackageName}:${fullVersion}`);
+    await exec(`docker push ${dockerPackageName}:${fullVersion}`);
+    console.log(`tag ${fullVersion} pushed`);
 }
 
 applyTags().catch(e => {
