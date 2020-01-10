15f819969104b5db0c8d7fa4537345f1db1c6d8c
Dekker <dekker25@gmail.com>
2020-01-10 18:50:06 +0300

    feat: more buckets for http requests

--- packages/server/server.ts
+++ packages/server/server.ts
@@ -30,7 +30,7 @@ const httpRequestDurationMicroseconds = new Histogram({
     name: 'http_request_duration_ms',
     help: 'Duration of HTTP requests in ms',
     labelNames: ['method', 'route', 'code'],
-    buckets: [0.1, 5, 15, 50, 100, 200, 300, 400, 500], // buckets for response time from 0.1ms to 500ms
+    buckets: [0.1, 5, 15, 50, 100, 200, 300, 400, 500, 1000, 1500, 2000, 5000, 10000], // buckets for response time from 0.1ms to 500ms
 });
 
 const compression = require('compression');
