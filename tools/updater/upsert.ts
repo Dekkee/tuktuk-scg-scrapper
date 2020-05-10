dad2c4b3861fc63d8e142a017816fd8ae4d282b9
Alexander Tihoniuk <dekker25@gmail.com>
2020-05-10 17:05:29 +0300

    fix: external update proxy (#41)

--- tools/updater/upsert.ts
+++ tools/updater/upsert.ts
@@ -1,18 +1,27 @@
 import { connect, disconnect } from '@tuktuk-scg-scrapper/storage/connect';
 import { Card } from '@tuktuk-scg-scrapper/storage/schemas/card';
 import fetch from 'node-fetch';
+import * as queue from 'async/queue';
+import * as Progress from 'progress';
 
+const { chain } = require('stream-chain');
 const { Writable } = require('stream');
+
+const fetchQueue = queue(async (value, callback) => {
+    const a = await fetch('https://scg.dekker.gdn/storage/card', {
+        method: 'PUT',
+        body: JSON.stringify(value),
+    });
+    console.log('done', value.name, a);
+    callback();
+}, 20);
 
 const uploadStream = new Writable({
     objectMode: true,
     autoDestroy: true,
     write: async function (chunk, encoding, callback) {
         try {
-            fetch('https://scg.dekker.gdn/storage/card', {
-                method: 'PUT',
-                body: chunk.value,
-            });
+            await fetchQueue.push(chunk);
         } catch (e) {
             console.error('Failed to update', e);
         } finally {
@@ -23,7 +32,22 @@ const uploadStream = new Writable({
 
 const upsertDatabase = async () => {
     await connect();
-    const pipeline = Card.find().cursor().pipe(uploadStream);
+    const total = await Card.find().count();
+    const progressBar = new Progress('-> uploading [:bar] :current/:total :percent :etas', {
+        width: 40,
+        complete: '=',
+        incomplete: ' ',
+        renderThrottle: 1,
+        total,
+    });
+    const pipeline = chain([
+        Card.find().cursor(),
+        (c) => {
+            progressBar.tick(1);
+            return c;
+        },
+        uploadStream,
+    ]);
     pipeline.on('end', async () => {
         await disconnect();
     });
